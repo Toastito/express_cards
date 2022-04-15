9ed78fa1aef71ffcee04996ce165458fbfe82ed4
Roger Davila <rogerddavila@gmail.com>
2022-04-15 12:35:02 -0700

    add delete and create functionality

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,10 +1,36 @@
 const Cards = require('../models/card');
 
 module.exports = {
-    index
+    index,
+    show,
+    new: newCard,
+    create,
+    delete: deleteCard
 }
 
 function index(req, res) {
     let cards = Cards.getAll();
     res.render('cards/', { cards })
-};
+}
+
+function show(req, res) {
+    console.log(req.params.id);
+    let card = Cards.getOne(req.params.id);
+    res.render('cards/show', { card });
+}
+
+function newCard(req, res) {
+    res.render('cards/new');
+}
+
+function create(req, res) {
+    const card = JSON.parse(JSON.stringify(req.body));
+    console.log(card);
+    Cards.create(card);
+    res.redirect('/cards');
+}
+
+function deleteCard(req, res) {
+    Cards.removeOne(req.params.id);
+    res.redirect('/cards');
+}
--- routes/cards.js
+++ routes/cards.js
@@ -1,10 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 const cardsCtrl = require('../controllers/cards');
 
 /* GET users listing. */
 router.get('/', cardsCtrl.index);
+// GET Add new card validation
+router.get('/new', cardsCtrl.new);
+// GET individual card
+router.get('/:id', cardsCtrl.show);
+// POST /card add new card
+router.post('/', cardsCtrl.create);
+// Delete /card/:id
+router.delete('/:id', cardsCtrl.delete);
 
 // router.get('/:id', cardsCtrl.show);
 
